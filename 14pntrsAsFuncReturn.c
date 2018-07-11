c018f75a30f21d983d24ae10d5ab35d5c80c5163
towfiq046 <towfiqahmed046@gmail.com>
2018-07-11 17:16:09 +0600

    stack problem

--- 14pntrsAsFuncReturn.c
+++ 14pntrsAsFuncReturn.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int add(int *a, int *b) {   // called function.
-    // a and b are pointer to integers local to add.
-    printf("address of 'a' in add = %d\n\n", &a);
-    printf("value in 'a' of add (address of 'a' in main) = %d\n\n", a);
-    printf("value at address stored in 'a' of add = %d\n\n", *a);
-    int c = (*a) + (*b);
-    return c;
+void printHelloWorld() {
+    printf("hello world.");
 }
 
-int main() {     // calling function.
-    int a = 2, b = 4, c;
-    printf("address of 'a' in main = %d\n\n", &a);   // just to show this a is different.
+int *add(int *a, int *b) {
+    int c = (*a) + (*b);
+    return &c;           // after execution in main, add() gets deallocated.
+                          // reason-- in stack add() stays at top so it gets deallocated.
+                          // can't return &c.
+}
 
-    // call by reference.
-    c = add(&a, &b);      // value in a, b of main is copied to a, b of add.
-    printf("result is = %d\n", c);
+int main() {
+    int a = 2, b = 4;
+
+    int *pntr = add(&a, &b);
+
+    printHelloWorld();
+
+    printf("result is = %d\n", *pntr);
 }
