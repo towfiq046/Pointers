9d5782b719a609feedcb87ffe5ddb66820a10d25
towfiq046 <towfiqahmed046@gmail.com>
2018-07-11 12:44:54 +0600

    commented

--- 14pntrsAsFuncReturn.c
+++ 14pntrsAsFuncReturn.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int add(int a, int b) {
+int add(int a, int b) {   // called function.
+    printf("address of a in add = %d\n", &a)   // just to show this a is different.
     int c = a + b;
     return c;
 }
 
-int main() {
+int main() {     // calling function.
     int a = 2, b = 4, c;
-    c = add(a, b);
+    printf("address of a in add = %d\n", &a)   // just to show this a is different.
+
+    // call by value.
+    c = add(a, b);      // value in a, b of main is copied to a, b of add.
     printf("result is = %d", c);
 }
