41b440c09583a86d373481108c4e41aba08ae626
towfiq046 <towfiqahmed046@gmail.com>
2018-07-11 13:01:42 +0600

    call by reference

--- 14pntrsAsFuncReturn.c
+++ 14pntrsAsFuncReturn.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int add(int a, int b) {   // called function.
-    printf("address of a in add = %d\n", &a)   // just to show this a is different.
-    int c = a + b;
+int add(int *a, int *b) {   // called function.
+    // a and b are pointer to integers local to add.
+    printf("address of 'a' in add = %d\n\n", &a);
+    printf("value in 'a' of add (address of 'a' in main) = %d\n\n", a);
+    printf("value at address stored in 'a' of add = %d\n\n", *a);
+    int c = (*a) + (*b);
     return c;
 }
 
 int main() {     // calling function.
     int a = 2, b = 4, c;
-    printf("address of a in add = %d\n", &a)   // just to show this a is different.
+    printf("address of 'a' in main = %d\n\n", &a);   // just to show this a is different.
 
-    // call by value.
-    c = add(a, b);      // value in a, b of main is copied to a, b of add.
-    printf("result is = %d", c);
+    // call by reference.
+    c = add(&a, &b);      // value in a, b of main is copied to a, b of add.
+    printf("result is = %d\n", c);
 }
